f026965622d8427aacf7cbc7275ef5c249437dde
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-02-11 17:51:40 +0800

    create custom function animate() to create a loop using recursion.

--- js/Main.js
+++ js/Main.js
@@ -2,3 +2,40 @@ const canvas = document.getElementById('canvas1');
 const canvasContext = canvas.getContext('2d'); // object built in in all browser has, this object contains all 2d drawing methods.
 
 //console.log(canvasContext); // check the object properties.
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+class Particle {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.size = 10; 
+        this.weight = 2;
+        this.directionX = 1;
+    }
+    
+    update(){
+        this.weight += 0.01;
+        this.y += this.weight;
+    }
+
+    draw(){
+        canvasContext.fillStyle = 'red';
+        canvasContext.beginPatch();
+        canvasContext.arc(this.x, this.y, this.size, 0, Math.PI * 2); // value in radiance that converts into 360 degrees. Full circle.
+        canvasContext.closePathc();
+        canvasContext.fill();
+    }
+
+}
+
+const particle1 = new Particle (100,10);
+
+function animate(){
+    particle1.update();
+    particle1.draw();
+    requestAnimationFrame(animate); // callling the custom function or parent function that will create a programming loop or programming concept called recursion.
+}// custom function
