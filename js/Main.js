0c2c9d6f72e9556c4037f386f8b12ad9d3378339
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-02-11 17:39:34 +0800

    create canvasContext object to access built in methods and console log to check object properties.

---
+++ js/Main.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById('canvas1');
+const canvasContext = canvas.getContext('2d'); // object built in in all browser has, this object contains all 2d drawing methods.
+
+//console.log(canvasContext); // check the object properties.
